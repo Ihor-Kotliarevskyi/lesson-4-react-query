8fc05a59d679e69c045d0969b911cb2c6968e91d
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-12-12 21:39:19 +0200

    add paginate adn formic

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,47 @@
-import axios from "axios";
+import ArticleList from "../ArticlesList/ArticlesList";
 import OrderForm from "../OrderForm/OrderForm";
 import "./App.module.css";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-
-const fetchPerson = async (id: string) => {
-  const response = await axios.get(`https://swapi.info/api/planets/${id}`);
-  console.log(response);
-
-  return response.data;
-};
+import SearchForm from "../SearchForm/SearchForm";
+import { fetchArticles, fetchPerson } from "../../services/Service";
+import ReactPaginate from "react-paginate";
+import css from "./App.module.css";
 
 function App() {
   const [id, setId] = useState("1");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [topic, setTopic] = useState("");
 
-  const { data, error, isLoading, isError } = useQuery({
+  const {
+    data: person,
+    error: personError,
+    isLoading: personIsLoading,
+    isError: personIsError,
+  } = useQuery({
     queryKey: ["person", id],
     queryFn: () => fetchPerson(id),
     enabled: Number(id) > 0,
   });
-  console.log(data);
+
+  const {
+    data: articles,
+    error: articlesError,
+    isLoading: articlesIsLoading,
+    isError: articlesIsError,
+  } = useQuery({
+    queryKey: ["articles", topic, currentPage],
+    queryFn: () => fetchArticles(topic, currentPage),
+    enabled: topic !== "",
+    placeholderData: keepPreviousData,
+  });
+
+  const totalPages = articles?.nbPages ?? 0;
+
+  const handleSearch = async (newTopic: string) => {
+    setTopic(newTopic);
+    setCurrentPage(1);
+  };
 
   return (
     <>
@@ -38,9 +60,35 @@ function App() {
       >
         Next
       </button>
-      {isLoading && <p>Loading...</p>}
-      {isError && <p>An error occurred: {error.message}</p>}
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      {personIsLoading && <p>Loading person ...</p>}
+      {personIsError && <p>An error occurred: {personError.message}</p>}
+      {person && <pre>{JSON.stringify(person, null, 2)}</pre>}
+      <br></br>
+      <SearchForm onSubmit={handleSearch} />
+      {totalPages > 0 && (
+        <ReactPaginate
+          pageCount={totalPages}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={1}
+          onPageChange={({ selected }) => setCurrentPage(selected + 1)}
+          forcePage={currentPage - 1}
+          containerClassName={css.pagination}
+          activeClassName={css.active}
+          nextLabel="→"
+          previousLabel="←"
+        />
+      )}
+
+      {articlesIsLoading && <p>Loading articles ...</p>}
+      {articlesIsError && (
+        <p>
+          Whoops, something went wrong! Please try again!:
+          {articlesError.message}
+        </p>
+      )}
+      {articles && articles.hits.length > 0 && (
+        <ArticleList items={articles.hits} />
+      )}
     </>
   );
 }
